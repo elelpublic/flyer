e46ee04d28224cfaa2ea60c6edd3d71590701042
CreativeDream <yuliangagarin@mail.ru>
2015-01-14 13:11:41 +0100

    fadeOut Update

--- src/main/webapp/sdk/sdk.js
+++ src/main/webapp/sdk/sdk.js
@@ -183,21 +183,36 @@
             var preloader = document.body.getElementsByClassName('preloader');
             
             if (preloader && preloader.length > 0) {
-                function fadeOut(el) {
-                    el.style.opacity = 1;
-                    var last = +new Date();
-                    var tick = function() {
-                        el.style.opacity = +el.style.opacity - (new Date() - last) / 400;
-                        last = +new Date();
-                        if (+el.style.opacity > 0) {
-                            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
-                        } else {
-                            preloader[0].parentNode.removeChild(preloader[0]);
+                function fadeOut(el, options) {
+                    var start = new Date;
+                    var id = setInterval(function() {
+                        var timePassed = new Date - start;
+                        var progress = timePassed / options.duration;
+                        if (progress > 1) {
+                            progress = 1;
                         }
-                    };
-                    tick();
+                        options.progress = progress;
+                        var delta = options.delta(progress);
+                        options.step(delta);
+                        if (progress == 1) {
+                            clearInterval(id);
+                            options.complete();
+                        }
+                    }, options.delay || 10);
                 }
-                fadeOut(preloader[0]);
+                fadeOut(preloader[0], {
+                    duration: 400,
+                    delta: function(progress) {
+                        progress = this.progress;
+                        return (0.5 - Math.cos(progress * Math.PI) / 2);
+                    },
+                    complete: function(){
+                        preloader[0].parentNode.removeChild(preloader[0]);
+                    },
+                    step: function(delta) {
+                        preloader[0].style.opacity = 1 - delta;
+                    }
+                });
             }
             
             if(event == "hide") { return; }
